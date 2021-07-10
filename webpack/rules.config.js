78d202de7a84862ca35a41a5baa829111c7c2b18
muhammad20 <muhammadahmad201196@hotmail.com>
2021-07-10 02:24:21 +0200

    first commit

---
+++ webpack/rules.config.js
@@ -0,0 +1,27 @@
+module.exports = [
+    {
+        test: /\.node$/,
+        use: 'node-loader',
+    },
+    {
+        test: /\.(m?js|node)$/,
+        parser: { amd: false },
+        use: {
+            loader: '@marshallofsound/webpack-asset-relocator-loader',
+            options: {
+                outputAssetBase: 'native_modules',
+            },
+        },
+    },
+    {
+        test: /\.(js|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+            loader: 'babel-loader'
+        }
+    },
+    {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+    },
+]
