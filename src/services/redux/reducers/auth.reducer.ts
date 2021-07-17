73fb4d33cfec9e715818f3f2bb9bf49dcdc63dbd
muhammad20 <muhammadahmadsherief@gmail.com>
2021-07-18 01:30:27 +0200

    auth reducer

---
+++ src/services/redux/reducers/auth.reducer.ts
@@ -0,0 +1,20 @@
+import { IAuthAction, LOGIN } from '../actions/authentication/auth-actions.interface';
+import { IAuthState } from '../states/auth.state';
+
+const initialState: IAuthState = {
+    token: null,
+    loginTimestamp: null
+};
+
+export const authReducer = (prevState = initialState, action: IAuthAction): IAuthState => {
+    switch (action.type) {
+    case LOGIN: {
+        return {
+            ...prevState,
+            token: action.token,
+            loginTimestamp: action.loginTimestamp
+        };
+    }
+    default: return prevState;
+    }
+};
