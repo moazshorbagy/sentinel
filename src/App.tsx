3264e3f986b6eba14c066e1b23a0f8ba909bbba1
muhammad20 <muhammadahmadsherief@gmail.com>
2021-07-18 01:30:36 +0200

    linting

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,48 @@
-import React from "react";
+import React from 'react';
 import {
-  HashRouter as Router,
-  Switch,
-  Route,
-  Redirect
-} from "react-router-dom";
-import { BottomNavigator } from "./components/bottom-navigator";
-import { IPageRoute } from "./components/bottom-navigator/props";
+    HashRouter as Router,
+    Switch,
+    Route,
+    Redirect
+} from 'react-router-dom';
+import { BottomNavigator } from './components/bottom-navigator';
+import { IPageRoute } from './components/bottom-navigator/props';
 
-import { TitleBar } from "./components/title-bar";
-import { LoginPage } from "./pages/login";
-import { TestPage } from "./pages/test-page";
+import { TitleBar } from './components/title-bar';
+import { LoginPage } from './pages/login';
+import { TestPage } from './pages/test-page';
 
 export const App: React.FC = () => {
-  const pages: IPageRoute[] = [
-    {
-      url: "/login",
-      name: "route 1",
-      key: 0,
-    },
-    {
-      url: "/test-page",
-      name: "route 3",
-      key: 2,
-    },
-  ];
+    const pages: IPageRoute[] = [
+        {
+            url: '/login',
+            name: 'route 1',
+            key: 0,
+        },
+        {
+            url: '/test-page',
+            name: 'route 3',
+            key: 2,
+        },
+    ];
 
-  return (
-    <>
-      <TitleBar />
-      <Router>
-        <Switch>
-          <Route exact path="/">
-            <Redirect to="/login" />
-          </Route>
-          <Route path="/login">
-            <LoginPage />
-          </Route>
-          <Route path="/test-page">
-            <TestPage />
-          </Route>
-        </Switch>
-        <BottomNavigator pages={pages} />
-      </Router>
-    </>
-  );
+    return (
+        <>
+            <TitleBar />
+            <Router>
+                <Switch>
+                    <Route exact path="/">
+                        <Redirect to="/login" />
+                    </Route>
+                    <Route path="/login">
+                        <LoginPage />
+                    </Route>
+                    <Route path="/test-page">
+                        <TestPage />
+                    </Route>
+                </Switch>
+                <BottomNavigator pages={pages} />
+            </Router>
+        </>
+    );
 };
