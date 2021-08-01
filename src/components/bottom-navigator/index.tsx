bc1d7f8a20128c3e92024b4c8e50b945129530fd
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2021-08-01 03:36:21 +0200

    Css color fixes

--- src/components/bottom-navigator/index.tsx
+++ src/components/bottom-navigator/index.tsx
@@ -10,19 +10,21 @@ export const BottomNavigator: React.FC<IBottomNavigatorProps> = (
     const [currentPage, setCurrentPage] = useState('');
     const linkClickHandler = (event: MouseEvent<HTMLAnchorElement>) => setCurrentPage(event.currentTarget.id);
     return (
-        <footer className="bottom-navigator primary">
+        <footer className="bottom-navigator">
             {props.pages &&
                 props.pages.map((page) => {
                     let className = 'navigator-btn';
                     if (currentPage == page.key.toString()) {
-                        className += ' p-dark';
+                        className += ' navigator-btn-active';
+                    } else {
+                        className += ' navigator-btn-inactive';
                     }
                     return (
                         <div key={page.key} className="navigation-btn-container">
                             <Link id={page.key.toString()} onClick={linkClickHandler} className={className} to={page.url}>
                                 <p className="navigator-btn-text">{page.name}</p>
                             </Link>
-                            <div className="separator p-dark"></div>
+                            <div className="separator"></div>
                         </div>
                     );
                 })}
