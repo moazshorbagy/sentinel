686836e09360db040849315ad4aa3e425ad138f6
Moaz Alaa El-Shorbagy <moaz_elshorbagy@hotmail.com>
2021-08-01 03:36:45 +0200

    Product mix empty page added

---
+++ src/pages/product-mix/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './product-mix.css';
+
+export const ProductMixPage: React.FC = () => {
+    return (
+        <div className='product-mix'>
+
+        </div>
+    );
+}
