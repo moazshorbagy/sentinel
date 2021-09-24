14189a4eec7af5dbbfd5369e80b55d3d0540073e
muhammad20 <muhammadahmad201196@hotmail.com>
2021-09-24 17:59:11 +0200

    added basic expected product mix sheet state actions

---
+++ src/services/redux/actions/product-mix/product-mix-actions.interface.ts
@@ -0,0 +1,49 @@
+/**
+ * Areas stored in the store are always in square meter
+ */
+
+/**
+ * Create, Update, Delete area division. E.g: residential area (a portion of the total land area)
+ */
+export const INITIALIZE_LAND_AREA = 'INITIALIZE_LAND_AREA';
+export const CREATE_AREA_DIVISION = 'CREATE_AREA_DIVISION';
+export const UPDATE_AREA_DIVISION = 'UPDATE_AREA_DIVISION';
+// not crucial
+const DELETE_AREA_DIVISION = 'DELETE_AREA_DIVISION';
+
+/**
+ * create, update, delete buildings
+ */
+export const CREATE_BUILDING = 'CREATE_BUILDING';
+export const UPDATE_BUILDING = 'UPDATE_BUILDING';
+// not crucial
+const DELETE_BUILDING = 'DELETE_BUILDING';
+
+/**
+ * Add a building unit to an existing building
+ */
+export const ADD_BUILDING_UNIT = 'ADD_BUILDING_UNIT';
+
+export interface InitializeLandAreaAction {
+    type: typeof INITIALIZE_LAND_AREA;
+    
+    // in square meter
+    totalArea: number;
+}
+
+export interface CreateAreaDivisionAction {
+    type: typeof CREATE_AREA_DIVISION;
+    name: string;
+
+    // area is a percentage of total area
+    areaPercentage: number;
+}
+
+export interface UpdateAreaDivisionAction {
+    type: typeof UPDATE_AREA_DIVISION;
+    name: string;
+    area: number;
+}
+
+
+export type ProductMixAction = InitializeLandAreaAction | CreateAreaDivisionAction | UpdateAreaDivisionAction;
